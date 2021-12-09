1bdfb97594ab301f6b9ed8cdd7065d261b00a5ab
tinhtrinh <chumeoconxauxa@gmail.com>
2021-12-09 14:51:04 +0700

    thêm pages home, login

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+            <Link to="/">Login</Link>
+        </div>
+    );
+}
+
+export default Home;
---
+++ frontend/src/pages/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const LogIn = () => {
+    return (
+        <div>
+            <h2>Login</h2>
+            <Link to="/home">Home</Link>
+        </div>
+    );
+}
+
+export default LogIn;
