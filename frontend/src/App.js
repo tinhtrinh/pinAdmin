3e56937df9c62ff7891e6ab44fa09786d4c347a6
Nguyen Toan <toannguyen@vngaynay.vn>
2021-12-10 10:32:07 +0700

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from "./pages/home";
 import Login from "./pages/login";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/home" component={Home} />
-      </Switch>
-    </Router>
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>Admin</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -21,15 +21,42 @@ const LogIn = () => {
           setErrMessage(err.message);
         });
     }
+    // return (
+    //     <div>
+    //         <Link to="/home">Home</Link>
+    //         <h2>Login</h2>
+    //         <h3>{errMessage}</h3>
+    //         <input placeholder="Tên đăng nhập" {...register('email')}/>
+    //         <input placeholder="Mật khẩu" {...register('password')}/>
+    //         <button onClick={handleSubmit(onSubmit)}>Đăng nhập</button>
+    //     </div>
+    // );
     return (
-        <div>
-            <Link to="/home">Home</Link>
-            <h2>Login</h2>
-            <h3>{errMessage}</h3>
-            <input placeholder="Tên đăng nhập" {...register('email')}/>
-            <input placeholder="Mật khẩu" {...register('password')}/>
-            <button onClick={handleSubmit(onSubmit)}>Đăng nhập</button>
-        </div>
+        <form>
+            <h3>Sign In</h3>
+
+            <div className="form-group" style={{marginBottom: '20px'}}>
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+            </div>
+
+            <div className="form-group" style={{marginBottom: '20px'}}>
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+            </div>
+
+            <div className="form-group" style={{marginBottom: '20px'}}>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+        </form>
     );
 }
 
